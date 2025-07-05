b4ac39cbe0385fe17d4731e814562496dad84e1e
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-07-05 12:19:19 +0100

    things/color: create

--- things/color/script.js
+++ things/color/script.js
@@ -1,17 +1,48 @@
 console.log(location.pathname);
 
+// this code is so bad, don't even try to fix it
+
 function onInput() {
-    const color = `rgb(${red.value}, ${green.value}, ${blue.value})`;
-    display.style.backgroundColor = localStorage.color = color;
+    saveValues();
+    display.style.backgroundColor = `rgb(${slideRed.value}, ${slideGreen.value}, ${slideBlue.value})`;
+
+    numberRed.innerText = slideRed.value;
+    numberGreen.innerText = slideGreen.value;
+    numberBlue.innerText = slideBlue.value;
+
+    numberRed.style.backgroundColor = `rgb(${slideRed.value}, 0, 0)`;
+    numberGreen.style.backgroundColor = `rgb(0, ${slideGreen.value}, 0)`;
+    numberBlue.style.backgroundColor = `rgb(0, 0, ${slideBlue.value})`;
 }
 
-red.addEventListener("input", onInput);
-green.addEventListener("input", onInput);
-blue.addEventListener("input", onInput);
+function saveValues() {
+    localStorage.red = slideRed.value;
+    localStorage.green = slideGreen.value;
+    localStorage.blue = slideBlue.value;
+}
 
-console.log(localStorage.color);
-if (localStorage.color) {
-    display.style.backgroundColor = localStorage.color
+function loadValues() {
+    display.style.backgroundColor = `rgb(${localStorage.red}, ${localStorage.green}, ${localStorage.blue})`
+    slideRed.value = numberRed.innerText = localStorage.red;
+    slideGreen.value = numberGreen.innerText = localStorage.green;
+    slideBlue.value = numberBlue.innerText = localStorage.blue;
+
+    numberRed.style.backgroundColor = `rgb(${localStorage.red}, 0, 0)`;
+    numberGreen.style.backgroundColor = `rgb(0, ${localStorage.green}, 0)`;
+    numberBlue.style.backgroundColor = `rgb(0, 0, ${localStorage.blue})`;
+}
+
+function copy() {
+    navigator.clipboard.writeText(display.style.backgroundColor)
+}
+window.copy = copy;
+
+slideRed.addEventListener("input", onInput);
+slideGreen.addEventListener("input", onInput);
+slideBlue.addEventListener("input", onInput);
+
+if (localStorage.red && localStorage.green && localStorage.blue) {
+    loadValues();
 } else {
     onInput();
 }

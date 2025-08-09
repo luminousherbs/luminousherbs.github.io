3abe0e1e5fdc6a60408bbdb023d7e6bbb782c4b8
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-08-09 18:53:36 +0100

    Create script.js

---
+++ things/time/script.js
@@ -0,0 +1,15 @@
+let count = 0;
+setInterval(function() {
+  const binary = count.toString(2).split("");
+  let index = 0;
+  for (let b of binary) {
+    if (document.getElementById(`div-${index}`)) {
+      document.getElementById(`div-${index}`).style.backgroundColor = (+b ? "yellow": "black");
+    } else {
+      const e = document.createElement("div");
+      e.id = `div-${index}`;
+      document.body.appendChild(e);
+    }
+  }
+  count++;
+}, 1)

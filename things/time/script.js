d9ab9a9916c14f14b3f13b0db7de5c251ed9119c
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-08-13 13:40:56 +0100

    format

--- things/time/script.js
+++ things/time/script.js
@@ -1,31 +1,31 @@
-
 const colors = {
-  on: "",
-  off: "#444444",
-}
+    on: "",
+    off: "#444444",
+};
 
 let count = 0;
 changeColor();
 
-setInterval(function() {
-  const binary = count.toString(2).split("");
-  console.log(binary);
-  let index = 0;
-  for (let b of binary) {
-    if (!document.getElementById(`div-${index}`)) {
-      console.log("making e");
-      const e = document.createElement("div");
-      e.id = `div-${index}`;
-      e.className = "lamp";
-      container.appendChild(e);
-      
+setInterval(function () {
+    const binary = count.toString(2).split("");
+    console.log(binary);
+    let index = 0;
+    for (let b of binary) {
+        if (!document.getElementById(`div-${index}`)) {
+            console.log("making e");
+            const e = document.createElement("div");
+            e.id = `div-${index}`;
+            e.className = "lamp";
+            container.appendChild(e);
+        }
+        document.getElementById(`div-${index}`).style.backgroundColor = +b
+            ? colors.on
+            : colors.off;
+        index++;
     }
-    document.getElementById(`div-${index}`).style.backgroundColor = (+b ? colors.on: colors.off);
-  index++;
-  }
-  count++;
+    count++;
 }, 250);
 
 function changeColor() {
-  colors.on = picker.value;
+    colors.on = picker.value;
 }

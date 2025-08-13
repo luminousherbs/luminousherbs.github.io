7373fc9978c3902b589a8227cf4295739f1627eb
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-08-13 11:16:00 +0100

    Create things/time

--- things/time/script.js
+++ things/time/script.js
@@ -1,19 +1,31 @@
-try {
+
+const colors = {
+  on: "",
+  off: "#444444",
+}
+
 let count = 0;
+changeColor();
+
 setInterval(function() {
   const binary = count.toString(2).split("");
+  console.log(binary);
   let index = 0;
   for (let b of binary) {
-    if (document.getElementById(`div-${index}`)) {
-      document.getElementById(`div-${index}`).style.backgroundColor = (+b ? "yellow": "black");
-    } else {
+    if (!document.getElementById(`div-${index}`)) {
+      console.log("making e");
       const e = document.createElement("div");
       e.id = `div-${index}`;
-      document.body.appendChild(e);
+      e.className = "lamp";
+      container.appendChild(e);
+      
     }
+    document.getElementById(`div-${index}`).style.backgroundColor = (+b ? colors.on: colors.off);
+  index++;
   }
   count++;
-}, 1)
-} catch (err) {
-  alert(err)
+}, 250);
+
+function changeColor() {
+  colors.on = picker.value;
 }

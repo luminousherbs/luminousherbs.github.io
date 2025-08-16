2bc783bee4aa37c09f819133df3aa6654d601137
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-08-16 11:43:46 +0100

    Update things/index.html to use feed

--- things/script.js
+++ things/script.js
@@ -1,93 +1,48 @@
 console.log(location.pathname);
 
-let allTags;
-let pageTags;
+// html elements
+feed;
 
-function filterPages() {
-    const allowedPages = new Set();
-    const choice = getFilterChoice(allTags);
-    for (const [key, value] of Object.entries(pageTags)) {
-        for (const c of choice.keys()) {
-            if (choice.get(c) && value.includes(c)) allowedPages.add(key);
-        }
-    }
-    for (const [key, value] of Object.entries(pageTags)) {
-        // special case where `getElementById` is required
-        document.getElementById(`link-${key}`).hidden = !allowedPages.has(key);
-    }
-}
-window.filterPages = filterPages;
 
-function selectAll(id) {
-    // special case where `getElementById` is required
-    // this could be refactored so we get passed an element instead of an id
-    const container = document.getElementById(id);
-    for (let box of container.querySelectorAll("input")) box.checked = true;
-    filterPages();
-}
-window.selectAll = selectAll;
+function createItem(item) {
 
-function selectNone(id) {
-    // special case where `getElementById` is required
-    const container = document.getElementById(id);
-    for (let box of container.querySelectorAll("input")) box.checked = false;
-    filterPages();
-}
-window.selectNone = selectNone;
+    const card = document.createElement("a");
+    card.className = "page-card";
+    card.href = item.querySelector("guid").textContent;
 
-function selectSome(id) {
-    // special case where `getElementById` is required
-    const container = document.getElementById(id);
-    // Math.random() returns a number between 0 and 1
-    // so Math.random() > 0.5 is a 50% chance to be true
-    for (let box of container.querySelectorAll("input")) box.checked = (Math.random() > 0.5);
-    filterPages();
-}
-window.selectSome = selectSome;
+    const title = document.createElement("h2");
+    title.innerText = item.querySelector("title").textContent;
+    
 
-async function getJSON(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    const description = document.createElement("p");
+    description.innerText = (
+        item.querySelector("description").textContent
+        +
+        "\n"
+        +
+        new Date(item.querySelector("pubDate").textContent).toLocaleDateString()
+    );
+
+    const br = document.createElement("br");
+
+    card.appendChild(title);
+    card.appendChild(description);
+    feed.appendChild(card);
+    feed.appendChild(br);
+
 }
 
-function getFilterChoice(filters) {
-    const choices = new Map()
-    for (let f of filters) {
-        // special case where `getElementById` is required
-        choices.set(f, document.getElementById(`filter-${f}`).checked);
-    }
-    return choices;
+function parseFeed() {
+    fetch("feed.xml")
+    .then((response) => response.text())
+    .then((str) => {
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(str, "application/xml");
+        const items = xml.querySelectorAll("item");
+        items.forEach((item) => {
+            createItem(item);
+        });
+  });
 }
 
-function getUniqueElementsFromValueArrays(obj) {
-    const elements = new Set();
-    for (const [key, value] of Object.entries(obj)) {
-        for (const v of value) {
-            elements.add(v)
-        }
-    }
-    return elements;
-}
-
-function createFilters(filters) {
-    for (let f of filters) {
-        filterBox.innerHTML += `<input type="checkbox" id="filter-${f}" onclick="filterPages()" checked>`;
-        filterBox.innerHTML += `<label for="filter-${f}">${f}</label><br>`;
-    }
-}
-
-function createLinkLayout(tags) {
-    for (const [key, value] of Object.entries(tags)) {
-        list.innerHTML += `<li id="link-${key}" style="margin: 20px 0;"><a href="/things/${key}"><strong>${key}</strong></a><br>(${value.join(", ")})`;
-    }
-}
-
-getJSON("/things/tags.json").then(result => {
-    pageTags = result;
-    createLinkLayout(result);
-    allTags = getUniqueElementsFromValueArrays(result);
-    createFilters(allTags);
-    filterPages();
-})
-
+parseFeed();

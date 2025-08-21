06ae2d17715422607d7581bd5a23dda56e442c4f
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-08-21 18:05:40 +0100

    Create module for parsing feeds

---
+++ assets/scripts/feed.js
@@ -0,0 +1,42 @@
+export async function parseFeed(category, showDescription = true, showDate = false) {
+
+    const elements = [];
+
+    return fetch(`${category}/feed.xml`)
+    .then((response) => response.text())
+    .then((str) => {
+
+        // convert the feed into javascript objects representing each item
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(str, "application/xml");
+        const items = xml.querySelectorAll("item");
+
+        items.forEach((item) => {
+
+            const card = document.createElement("a");
+            card.className = "page-card";
+            card.href = item.querySelector("guid").textContent;
+
+            const title = document.createElement("h2");
+            title.innerText = item.querySelector("title").textContent;
+            
+            
+            const subheading = document.createElement("p");
+
+            if (showDescription) {
+                subheading.innerText += item.querySelector("description").textContent;
+            }
+
+            if (showDate) {
+                subheading.innerText += new Date(item.querySelector("pubDate").textContent).toLocaleDateString()
+            }
+
+
+            card.appendChild(title);
+            card.appendChild(subheading);
+            elements.push(card);
+        });
+        return elements;
+    });
+
+}

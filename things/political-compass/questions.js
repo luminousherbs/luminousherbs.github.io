7010a7c010eee1c204ff225219f5905e60d5c68c
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-08-25 16:58:19 +0100

    Create questions.js

---
+++ things/political-compass/questions.js
@@ -0,0 +1,243 @@
+export const questions = [
+  {
+    question: "What is your favorite natural element?",
+    options: [
+      {
+        title: "Earth",
+        image: "/assets/images/earth.jpg",
+        scores: {
+          cool: +1,
+          mysterious: -1,
+        },
+      },
+      {
+        title: "Air",
+        image: "/assets/images/air.jpg",
+        scores: {
+          cool: +1,
+          musical: +1,
+        },
+      },
+      {
+        title: "Fire",
+        image: "/assets/images/fire.jpg",
+        scores: {
+          cool: -3,
+          mysterious: +1,
+          musical: +2,
+        },
+      },
+      
+      {
+        title: "Water",
+        image: "/assets/images/water.jpg",
+        scores: {
+          cool: +2,
+          mysterious: -1,
+        },
+      },
+    ],
+  },
+  {
+    question: "What would you do with $1 million dollar?",
+    options: [
+      {
+        title: "Family",
+        image: "/assets/images/family.jpg",
+        scores: {
+          cool: -2,
+          mysterious: -2,
+        },
+      },
+      {
+        title: "Friends",
+        image: "/assets/images/friends.jpg",
+        scores: {
+          cool: +2,
+          musical: -2,
+        },
+      },
+      {
+        title: "Enemies",
+        image: "/assets/images/enemies.jpg",
+        scores: {
+          cool: +1,
+          mysterious: +3,
+        },
+      },
+      
+      {
+        title: "Strangers",
+        image: "/assets/images/strangers.jpg",
+        scores: {
+          cool: +1,
+          mysterious: +4,
+        },
+      },
+    ],
+  },
+  {
+    question: "What superpower would you have?",
+    options: [
+      {
+        title: "Flight",
+        image: "/assets/images/flying.jpg",
+        scores: {},
+      },
+      {
+        title: "Invisibility",
+        image: "/assets/images/invisible.jpg",
+        scores: {
+          mysterious: +4,
+        },
+      },
+      {
+        title: "Super strong",
+        image: "/assets/images/strong.jpg",
+        scores: {
+          cool: +2,
+        },
+      },
+      {
+        title: "Super weak",
+        image: "/assets/images/weak.jpg",
+        scores: {
+          cool: -2,
+        },
+      },
+      {
+        title: "Turning into a mushroom (you can't turn back)",
+        image: "/assets/images/mushroom.jpg",
+        scores: {
+          mysterious: -2,
+        },
+      },
+      {
+        title: "X-ray vision",
+        image: "/assets/images/x-ray.jpg",
+        scores: {
+          cool: +1,
+          mysterious: +1,
+        },
+      },
+    ],
+  },
+  {
+    question: "I tend to cause chaos in the streets",
+    options: [
+      {
+        title: "Agree",
+        image: "/assets/images/yes.jpg",
+        scores: {
+          cool: +1,
+          mysterious: +1
+        },
+      },
+      {
+        title: "Somewhat disagree",
+        image: "/assets/images/no.jpg",
+        scores: {},
+      },
+      {
+        title: "Somewhat agree",
+        image: "/assets/images/yes2.jpg",
+        scores: {},
+      },
+      {
+        title: "Disagree",
+        image: "/assets/images/no2.jpg",
+        scores: {
+          cool: -2,
+        },
+      },
+      {
+        title: "Neither agree nor disagree",
+        image: "/assets/images/coward.jpg",
+        scores: {
+          cool: -3,
+          mysterious: +1,
+        },
+      },
+      {
+        title: "Both agree and disagree",
+        image: "/assets/images/shrug.jpg",
+        scores: {
+          mysterious: +3,
+        },
+      },
+    ],
+  },
+  {
+    question: "What is your favorite Tesco feature?",
+    options: [
+      {
+        title: "Clock tower",
+        image: "/assets/images/clock-tower.jpg",
+        scores: {
+          cool: -1,
+          musical: +1,
+        },
+      },
+      {
+        title: "Clubcard propaganda",
+        image: "/assets/images/clubcard.jpg",
+        scores: {
+          cool: -1,
+          mysterious: +1,
+        },
+      },
+      {
+        title: "Community voting",
+        image: "/assets/images/tesco-voting.jpg",
+        scores: {
+          cool: -1,
+        },
+      },
+      {
+        title: "Photo ID booth",
+        image: "/assets/images/photo-id.jpg",
+        scores: {
+          cool: +1,
+        },
+      },
+    ],
+  },
+  {
+    question: "Is 1 a prime number?",
+    options: [
+      {
+        title: "Yes",
+        image: "/assets/images/yes.jpg",
+        scores: {
+          cool: +1,
+          mysterious: +1,
+        },
+      },
+      {
+        title: "No",
+        image: "/assets/images/no.jpg",
+        scores: {
+          mysterious: -1,
+        },
+      },
+      {
+        title: "Don't know",
+        image: "/assets/images/coward.jpg",
+        scores: {
+          cool: -1,
+          mysterious: +1,
+          musical: -1,
+        },
+      },
+      {
+        title: "Don't care",
+        image: "/assets/images/shrug.jpg",
+        scores: {
+          cool: +1,
+          mysterious: +2,
+          musical: -1,
+        },
+      },
+    ],
+  },
+];

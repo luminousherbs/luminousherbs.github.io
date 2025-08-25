6f1885db72352433d1e18390ff031bb998994e21
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-08-25 17:02:52 +0100

    Update script.js

--- things/political-compass/script.js
+++ things/political-compass/script.js
@@ -1,1 +1,13 @@
-console.log(location.pathname)
+import { questions } from "questions.js";
+
+console.log(location.pathname);
+
+const userScores = {
+  cool: 0,
+  mysterious: 0,
+  musical: 0,
+};
+
+for (let q of questions) {
+  alert(q.question)
+}

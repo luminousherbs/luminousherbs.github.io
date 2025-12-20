e7948b857489373ff9336a8f53b4116729199222
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-12-20 13:20:46 +0000

    shifty-scale: use objects ins tead of maps and extract scores into module

---
+++ things/shifty-scale/answers.js
@@ -0,0 +1,45 @@
+export const answers = {
+    0: {
+        score: "medium",
+        explanation: "Too confusing to make a clear judgement.",
+    },
+    1: {
+        score: "well done",
+        explanation: "Pretty shifty if you ask me.",
+    },
+    2: {
+        score: "medium rare",
+        explanation:
+            "That's not shifty, wearing a clown mask just improves his programming abilities.",
+    },
+    3: {
+        score: "blue",
+        explanation: "He's living his best life.",
+    },
+    4: {
+        score: "overcooked",
+        explanation: "That's textbook shifty right there.",
+    },
+    5: {
+        score: "overcooked",
+        explanation:
+            "Will all great Neptune's oceans wash this shift clean from my hands?",
+    },
+    6: {
+        score: "medium well",
+        explanation: "That's a shifty expression but not a shifty shirt.",
+    },
+    7: {
+        score: "rare",
+        explanation: "Nothing shifty about this.",
+    },
+    8: {
+        score: "well done",
+        explanation: "Doing the YMCA while high is a clear indicator of shift.",
+    },
+    9: {
+        score: "rare",
+        explanation:
+            "Don't judge him, his hoodie was surgically attached to him in a freak accident.",
+    },
+};
--- things/shifty-scale/script.js
+++ things/shifty-scale/script.js
@@ -1,64 +1,33 @@
 import { randomInteger } from "/assets/scripts/random.js";
+import { answers } from "./answers.js";
 
 console.log(location.pathname);
 
-const celsiusToDoneness = new Map([
-    [46, "blue"],
-    [52, "rare"],
-    [55, "medium rare"],
-    [60, "medium"],
-    [65, "medium well"],
-    [69, "well done"],
-    [71, "overcooked"],
-]);
-
-const shiftyScores = new Map([
-    [0, "medium"],
-    [1, "well done"],
-    [2, "medium rare"],
-    [3, "blue"],
-    [4, "overcooked"],
-    [5, "overcooked"],
-    [6, "medium well"],
-    [7, "rare"],
-    [8, "well done"],
-    [9, "rare"],
-]);
-
-const shiftyExplanations = new Map([
-    [0, "Too confusing to make a clear judgement."],
-    [1, "Pretty shifty if you ask me."],
-    [
-        2,
-        "That's not shifty, wearing a clown mask just improves his programming abilities.",
-    ],
-    [3, "He's living his best life."],
-    [4, "That's textbook shifty right there."],
-    [5, "Will all great Neptune's oceans wash this shift clear from my hands?"],
-    [6, "That's a shifty expression but not a shifty shirt."],
-    [7, "Nothing shifty about this."],
-    [8, "Doing the YMCA while high is a clear indicator of shift."],
-    [
-        9,
-        "Don't judge him, his hoodie was surgically attached to him in a freak accident.",
-    ],
-]);
+const celsiusToDoneness = {
+    46: "blue",
+    52: "rare",
+    55: "medium rare",
+    60: "medium",
+    65: "medium well",
+    69: "well done",
+    71: "overcooked",
+};
 
 let imageIndex;
 
-function flipMap(map) {
-    const newMap = new Map();
-    for (const [key, value] of map) {
-        newMap.set(value, key);
+function flipObject(object) {
+    const flippedObject = {};
+    for (const [key, value] of Object.entries(object)) {
+        flippedObject[value] = key;
     }
-    return newMap;
+    return flippedObject;
 }
 
-function convertUsingLowerLimit(key, map) {
+function convertUsingLowerLimit(key, object) {
     key = Math.floor(key);
     while (true) {
-        if (key < Math.min(map.keys())) return undefined;
-        let value = map.get(key);
+        if (key < Math.min(Object.keys(object))) return undefined;
+        let value = object[key];
         if (value !== undefined) {
             return value;
         } else {
@@ -81,7 +50,7 @@ function onTemperatureInput() {
 
 function onDonenessInput() {
     fieldTemperature.value =
-        flipMap(celsiusToDoneness).get(fieldDoneness.value) ??
+        flipObject(celsiusToDoneness)[fieldDoneness.value] ??
         fieldTemperature.value;
     if (fieldDoneness.value == "blue") {
         document.body.style.color = "blue";
@@ -98,9 +67,11 @@ function randomPhoto() {
 }
 
 function onSubmit() {
+    const answer = answers[imageIndex];
+
     submit.disabled = true;
-    answer.innerText = `Correct answer: ${shiftyScores.get(imageIndex)}.`;
-    explanation.innerText = shiftyExplanations.get(imageIndex);
+    answerField.innerText = `Correct answer: ${answer.score}.`;
+    explanationField.innerText = answer.explanation;
     postgame.hidden = false;
 }
 

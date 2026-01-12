433021ab35244b6cabe78c0ecc340219cf4b7371
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2026-01-12 19:36:55 +0000

    Build xml and js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPassthroughCopy("**/feed.xml");
+    eleventyConfig.addPassthroughCopy("**.xml");
+    eleventyConfig.addPassthroughCopy("**.js");
 };

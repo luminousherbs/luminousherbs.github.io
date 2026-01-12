28f00b87694cb1d1d4f9ef1494649d77ef532a51
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2026-01-12 21:01:49 +0000

    Update things

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPassthroughCopy("**.xml");
-    eleventyConfig.addPassthroughCopy("**.js");
+    eleventyConfig.addPassthroughCopy("**/*.xml");
+    eleventyConfig.addPassthroughCopy("**/*.js");
 };
